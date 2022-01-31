abf4fef27f4433420bab0a45a78515da613424ae
Aman <amanks20@iitk.ac.in>
2022-02-01 02:12:47 +0530

    add base.js

---
+++ base.js
@@ -0,0 +1,18 @@
+async function GetQuote() {
+
+    await fetch('https://quote-garden.herokuapp.com/api/v3/quotes')
+        .then(response => response.json())
+        .then(data => {
+
+            console.log(data);
+            let i = Math.floor(Math.random() * 10);
+            let quote1 = data['data'][i]['quoteText'];
+            let author = data['data'][i]['quoteAuthor'];
+
+
+            document.getElementById('quote').innerHTML = '"' + quote1 + '"';
+            document.getElementById('author').innerHTML = `~` + author;
+        })
+
+
+}
